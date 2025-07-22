ecb21ffcd32cd296cf37a11b5a7d3e7b07a8c8f8
fucadzi <fucadzi@gmail.com>
2025-07-22 19:48:06 +0300

    feat: balance list

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import { BalanceList } from '@/components/BalanceList';
+
 export default function Home() {
     return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <section className="flex flex-col items-center justify-center gap-8 py-8 md:py-10">
             <div className="inline-block max-w-xl text-center justify-center">
-                <h1 className="text-4xl font-bold">CoinKeep</h1>
+                <h1 className="text-4xl font-bold">Welcome to CoinKeep!</h1>
             </div>
 
-            <div className="mt-8"></div>
+            <div className="w-full max-w-4xl">
+                <BalanceList />
+            </div>
         </section>
     );
 }
