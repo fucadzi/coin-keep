58c450fe2fe55214eb1e0ba56cb5099b28bd36f4
fucadzi <fucadzi@gmail.com>
2025-07-22 22:16:34 +0300

    chore: cleanup

--- app/error.tsx
+++ app/error.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
     useEffect(() => {
         // Log the error to an error reporting service
-        /* eslint-disable no-console */
+
         console.error(error);
     }, [error]);
 
